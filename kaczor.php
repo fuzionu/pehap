b687eb7dddad6c4e0c78219a6632c13dd5289c69
fuzionu <fuzionu@gmail.com>
2020-12-27 23:34:22 +0100

    add filling list & showing names

--- kaczor.php
+++ kaczor.php
@@ -1,23 +1,26 @@
 <?php
 
-$list = [
-	'Kamil',
-	'Daniel',
-	'Mateusz',
-	'Eryk',
-	'Gieka',
-	'Anna',
-	'Maks',
-	'Piotr',
-	'Agnieszka',
-	'Jakub',
-	'Ryszard',
-	'Kinga',
-	'Karolina'
-];
+require("vendor\autoload.php");
 
-foreach ($list as $name)
-{
-	echo 'Imie: '.$name."\n";
+function fillList()
+{	
+	$faker = \Faker\Factory ::create();
+	
+	for ($x = 0; $x < 20; $x++)
+	{
+		$list[$x] = $faker->name;
+	}
+	
+	return $list;
 }
+function showNames()
+{
+	$list = fillList(20);
+	foreach ($list as $name)
+	{
+		echo 'Name: '.$name."\n";
+	}
+}
+
+showNames();
 ?>
