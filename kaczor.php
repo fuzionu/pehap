81f9220c77b52c193f4db8576781fbe93098ea07
fuzionu <fuzionu@gmail.com>
2020-12-28 04:08:01 +0100

    add handling mixed letters/digits strings

--- kaczor.php
+++ kaczor.php
@@ -2,6 +2,20 @@
 
 $file = './daniel.txt';
 
+function splitString($string)
+{
+	$numbers = preg_replace('/[^0-9]/', '', $string);
+	$letters = preg_replace('/[^a-zA-Z]/', '', $string);
+	
+	$splittedValuesArray = [$letters, $numbers];
+	
+	return $splittedValuesArray;
+}
+
+function isMixedString($string)
+{
+    return preg_match('/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/', $string);
+}
 
 function getContents($file)
 {
@@ -11,15 +25,19 @@ function getContents($file)
 	{
 		return false;
 	}
-	else
-	{
-		return $fileContents;
-	}
+
+	return $fileContents;
 }
 
 function processToUppercase($fileContents)
 {
 	return strtoupper($fileContents);
+}
+
+function saveUppercaseToFile($file, $fileContents)
+{
+	file_put_contents($file, processToUppercase($fileContents));
+	echo 'Contents of the file processed to uppercase';	
 }
 
 function handleFileContents($file, $fileContents)
@@ -28,14 +46,35 @@ function handleFileContents($file, $fileContents)
 	{
 		echo 'File is empty';
 	}
+	
+	else if (ctype_upper($fileContents) == true)
+	{
+		echo 'File contents already in uppercase';
+	}
+	
 	else if (is_numeric($fileContents))
 	{
-		echo 'File contains digits only, nothing to process';
+		echo 'File contains digits only';
 	}
+	
+	else if (isMixedString($fileContents) == true)
+	{
+		$lettersAndDigits = splitString($fileContents);
+		
+		if (ctype_upper($lettersAndDigits[0]))
+		{
+			echo 'File contents already in uppercase';
+		}
+		
+		else
+		{
+			saveUppercaseToFile($file, $fileContents);
+		}
+	}
+	
 	else
 	{
-		file_put_contents($file, processToUppercase($fileContents));
-		echo 'Contents of file processed to upper case';
+		saveUppercaseToFile($file, $fileContents);
 	}
 }
 
