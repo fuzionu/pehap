720475c6ace411f2db01789fd7abaf939df55d24
fuzionu <fuzionu@gmail.com>
2020-12-30 23:46:12 +0100

    add showing info when successfully processed letters

--- kaczor.php
+++ kaczor.php
@@ -4,6 +4,7 @@ if (file_exists('daniel.txt'))
 {
 	$fileContents = file_get_contents('daniel.txt');
 	file_put_contents('daniel.txt', strtoupper($fileContents));
+	echo 'Letters processed to uppercase';
 }
 else
 {
