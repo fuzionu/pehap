7f58686aa454badbdfe2f6aed4ce9a3eb81926f0
fuzionu <fuzionu@gmail.com>
2020-12-30 23:49:13 +0100

    change message for successful processing

--- kaczor.php
+++ kaczor.php
@@ -4,7 +4,7 @@ if (file_exists('daniel.txt'))
 {
 	$fileContents = file_get_contents('daniel.txt');
 	file_put_contents('daniel.txt', strtoupper($fileContents));
-	echo 'Letters processed to uppercase';
+	echo 'File processed successfully';
 }
 else
 {
