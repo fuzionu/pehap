5273eed417358c5be512b91944a868e71723260f
fuzionu <fuzionu@gmail.com>
2021-01-11 18:06:38 +0100

    delete all var_dumps

--- kaczor.php
+++ kaczor.php
@@ -4,21 +4,15 @@ require("vendor\autoload.php");
 
 if (file_exists('daniel.json'))
 {	
-	$faker = \Faker\Factory ::create();
-			
+	$faker = \Faker\Factory ::create();			
 	$fileContents = file_get_contents('daniel.json');
 	$decodedJson = json_decode($fileContents, true);
-	var_dump($decodedJson);
 	$innerArray = $decodedJson['names'];
-	var_dump($innerArray);
 	$innerArray[] = $faker->name;
 	$decodedJson['names'] = $innerArray;
-	var_dump($decodedJson);
 	$encodedJson = json_encode($decodedJson);
-	var_dump($encodedJson);
 	file_put_contents('daniel.json', $encodedJson);
-	
-	
+		
 	echo 'Added name to the file';
 }
 
