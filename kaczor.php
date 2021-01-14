ca93a8b5817668b71f2c06456d3ee3c1e6a51a97
fuzionu <fuzionu@gmail.com>
2021-01-14 22:22:46 +0100

    move $decodedJson['names'] to countAndAdd argument

--- kaczor.php
+++ kaczor.php
@@ -8,8 +8,9 @@ function countAndAdd($array)
 	
 	if (count($array) % 2 === 0)
 	{
-		echo 'Added name to the file';
 		$array[] = $faker->name;		
+		echo 'Added name to the file';
+		
 		return $array;
 	}
 	
@@ -22,13 +23,10 @@ function countAndAdd($array)
 	return $array;
 }
 
-
 if (file_exists('daniel.json'))
 {	
-	$decodedJson = json_decode(file_get_contents('daniel.json'), true);
-	$innerArray = $decodedJson['names'];
-	
-	$innerArray = countAndAdd($innerArray);
+	$decodedJson = json_decode(file_get_contents('daniel.json'), true);	
+	$innerArray = countAndAdd($decodedJson['names']);
 	
 	$decodedJson['names'] = $innerArray;
 	file_put_contents('daniel.json', json_encode($decodedJson));
