d07ae797e9335e4441db116ffaf20af5aa3be738
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-01-14 22:35:44 +0100

    Add type hints

--- kaczor.php
+++ kaczor.php
@@ -2,32 +2,32 @@
 
 require("vendor\autoload.php");
 
-function countAndAdd($array)
+function countAndAdd(array $array): array
 {
-	$faker = \Faker\Factory ::create();	
-	
+	$faker = \Faker\Factory ::create();
+
 	if (count($array) % 2 === 0)
 	{
-		$array[] = $faker->name;		
+		$array[] = $faker->name;
 		echo 'Added name to the file';
-		
+
 		return $array;
 	}
-	
+
 	for ($i = 0; $i < 3; $i++)
 	{
 		$array[] = $faker->name;
 	}
-	
+
 	echo 'Added three names to the file';
 	return $array;
 }
 
 if (file_exists('daniel.json'))
-{	
-	$decodedJson = json_decode(file_get_contents('daniel.json'), true);	
+{
+	$decodedJson = json_decode(file_get_contents('daniel.json'), true);
 	$innerArray = countAndAdd($decodedJson['names']);
-	
+
 	$decodedJson['names'] = $innerArray;
 	file_put_contents('daniel.json', json_encode($decodedJson));
 }
