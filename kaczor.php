b629802d9724255ffac97f50ba2999f18c268dc7
fuzionu <fuzionu@gmail.com>
2021-01-14 22:21:22 +0100

    add appending 1 or 3 names depending on names count

--- kaczor.php
+++ kaczor.php
@@ -2,16 +2,36 @@
 
 require("vendor\autoload.php");
 
+function countAndAdd($array)
+{
+	$faker = \Faker\Factory ::create();	
+	
+	if (count($array) % 2 === 0)
+	{
+		echo 'Added name to the file';
+		$array[] = $faker->name;		
+		return $array;
+	}
+	
+	for ($i = 0; $i < 3; $i++)
+	{
+		$array[] = $faker->name;
+	}
+	
+	echo 'Added three names to the file';
+	return $array;
+}
+
+
 if (file_exists('daniel.json'))
 {	
-	$faker = \Faker\Factory ::create();	
 	$decodedJson = json_decode(file_get_contents('daniel.json'), true);
 	$innerArray = $decodedJson['names'];
-	$innerArray[] = $faker->name;
+	
+	$innerArray = countAndAdd($innerArray);
+	
 	$decodedJson['names'] = $innerArray;
 	file_put_contents('daniel.json', json_encode($decodedJson));
-		
-	echo 'Added name to the file';
 }
 
 else
