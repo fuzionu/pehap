b3dcd8aaad9a49a9aceae9f3f5f4f1b5290ed9e8
fuzionu <fuzionu@gmail.com>
2021-01-16 20:06:30 +0100

    add appending names at random position

--- kaczor.php
+++ kaczor.php
@@ -2,32 +2,22 @@
 
 require("vendor\autoload.php");
 
-function countAndAdd(array $array): array
+function drawAndAdd(array $array): array
 {
 	$faker = \Faker\Factory ::create();
-
-	if (count($array) % 2 === 0)
-	{
-		$array[] = $faker->name;		
-		echo 'Added name to the file'."\n";
-		
-		return $array;
-	}
-
-	for ($i = 0; $i < 3; $i++)
-	{
-		$array[] = $faker->name;
-	}
+	$randNum = rand(0, count($array)-1);	
+	$array[$randNum] = $faker->name;
 	
-	echo 'Added three names to the file'."\n";
-
+	$randNum += 1;
+	echo 'Added name at field '.$randNum;
+	
 	return $array;
 }
 
 if (file_exists('daniel.json'))
 {
 	$decodedJson = json_decode(file_get_contents('daniel.json'), true);
-	$innerArray = countAndAdd($decodedJson['names']);
+	$innerArray = drawAndAdd($decodedJson['names']);
 
 	$decodedJson['names'] = $innerArray;
 	file_put_contents('daniel.json', json_encode($decodedJson));
